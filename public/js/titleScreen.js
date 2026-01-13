d79a5af59d7eb7b7f5a60549b58bf02c04327b7b
NexChris <ngabalachris@gmail.com>
2026-01-13 20:19:17 +0100

    feat: updating local version

--- public/js/titleScreen.js
+++ public/js/titleScreen.js
@@ -7,13 +7,24 @@ ctx.font = "20px SEGA";
 ctx.fillStyle = "white";
 
 const backgroundImage = new Image();
-const Clickaudio = new Audio("../music//sfx/click.wav");
-
-backgroundImage.src = "/image/background/titleScreen.png"
+const Clickaudio = new Audio("../audio/sfx/click.wav");
 
 backgroundImage.onload = () => {
+  console.log("✅ Background chargé !");
   titleScreen();
 };
+backgroundImage.onerror = () => {
+  console.error("❌ Impossible de charger le background !");
+};
+
+Clickaudio.addEventListener("canplaythrough", () => {
+  console.log("✅ Audio Click chargé !");
+});
+Clickaudio.onerror = () => {
+  console.error("❌ Impossible de charger l'audio Click !");
+};
+
+backgroundImage.src = "../image/background/titleScreen.png";
 
 function titleScreen() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
